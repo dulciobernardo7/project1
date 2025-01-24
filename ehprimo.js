62ad2182c71f3394253619975e787084de26b724
dulciobernardo7 <dulciobernardo90@gmail.com>
2025-01-24 02:08:30 +0100

    Mexendo nos dois arquivos

--- ehprimo.js
+++ ehprimo.js
@@ -1,10 +1,10 @@
-let num = 6;
+let num = 1;
 
 if(num < 2){
   console.log(`O numero ${num} nao e primo`);
 }
-for(let counter = 2;counter * counter == num;counter++){
-    if(counter % num == 2){
+for(let counter = 2;counter * counter <= num;counter++){
+    if(num % counter === 0){
       console.log(`O numero ${num} nao e primo`);
     }
 }
