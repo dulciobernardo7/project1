75e29fc6671d4758b2e8b0a439302d71d6e0f66f
dulciobernardo7 <dulciobernardo90@gmail.com>
2025-01-24 02:35:15 +0100

    terminando o exercicios

--- ehprimo.js
+++ ehprimo.js
@@ -1,10 +1,14 @@
-let num = 1;
+let num = 6;
 
 if(num < 2){
   console.log(`O numero ${num} nao e primo`);
 }
-for(let counter = 2;counter * counter <= num;counter++){
-    if(num % counter === 0){
-      console.log(`O numero ${num} nao e primo`);
+else{
+    for(let counter = 2;counter * counter <= num;counter++){
+        if(num % counter === 0){
+          console.log(`O numero ${num} nao e primo`);
+          return;
+        }
     }
+    console.log(`O numero ${num} e primo`);
 }
